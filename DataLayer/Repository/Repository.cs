a276ed932afd49c0a5b3c9ef15b772ef22453602
jirij <jirij@DESKTOP-ITUH371>
2021-04-10 19:47:22 +0200

    Added abstraction

---
+++ DataLayer/Repository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository
+{
+    public class Repository<T> where T : class
+    {
+        protected readonly LibraryDbContext libraryDbContext;
+
+        public Repository(LibraryDbContext libraryDbContext)
+        {
+            this.libraryDbContext = libraryDbContext;
+        }
+
+        //Read
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await libraryDbContext.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await libraryDbContext.Set<T>().FindAsync(id);
+        }
+        //Create
+        public async Task CreateAsync(T entity)
+        {
+            await libraryDbContext.Set<T>().AddAsync(entity);
+            await libraryDbContext.SaveChangesAsync();
+        }
+        public async Task CreateManyAsync(List<T> entities)
+        {
+            libraryDbContext.Set<T>().AddRange(entities);
+            await libraryDbContext.SaveChangesAsync();
+        }
+        //Patch
+        public async Task UpdateAsync(T entity)
+        {
+            libraryDbContext.Entry(entity).State = EntityState.Modified;
+            await libraryDbContext.SaveChangesAsync();
+        }
+        //Delete
+        public async Task DeleteAsync(T entity)
+        {
+            libraryDbContext.Set<T>().Remove(entity);
+            await libraryDbContext.SaveChangesAsync();
+        }
+        public async Task DeleteManyAsync(List<T> entity)
+        {
+
+            libraryDbContext.Set<T>().RemoveRange(entity);
+            await libraryDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ library/Schema/Publisher/PublisherMutations.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Models;
+using DataLayer.Repository;
+using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Schema.Publisher
+{
+    [ExtendObjectType("MutationQuery")]
+    public class PublisherMutations
+    {
+        private readonly PublisherRepository publisherRepository;
+
+        public PublisherMutations(PublisherRepository publisherRepository)
+        {
+            this.publisherRepository = publisherRepository;
+        }
+
+        public async Task<PublisherModel> CreatePublisher(PublisherModel publisherModel)
+        {
+            await publisherRepository.CreateAsync(publisherModel);
+            return publisherModel;
+        }
+    }
+}
