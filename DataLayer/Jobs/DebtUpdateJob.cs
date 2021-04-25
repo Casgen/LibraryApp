d5d72099c10736e50bd567295acbd4a9a7399393
Vítězslav Zotov <vzotov42@gmail.com>
2021-04-25 14:36:34 +0200

    Renamed Services to Jobs

---
+++ DataLayer/Jobs/DebtUpdateJob.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Jobs
+{
+    public class DebtUpdateJob : IJob
+    {
+        private readonly IDbContextFactory<LibraryDbContext> dbContextFactory;
+        public DebtUpdateJob(IDbContextFactory<LibraryDbContext> dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await using LibraryDbContext libraryDbContext = dbContextFactory.CreateDbContext();
+            var reservation = libraryDbContext.Reservations.Find(context.JobDetail.JobDataMap.Get("reservationId"));
+
+            if (reservation.BookReturned)
+            {
+                reservation.Debt = 0;
+                await context.Scheduler.DeleteJob(context.JobDetail.Key);
+            }
+            else
+            {
+                var remainingDate = (int)Math.Floor(DateTime.Now.Subtract(reservation.DateTo).TotalDays);
+                int debt = (int)Math.Ceiling((double)remainingDate / 31) * 60;
+                reservation.Debt = remainingDate > 0 ? debt : 0;
+            }
+            libraryDbContext.Reservations.Update(reservation);
+            await libraryDbContext.SaveChangesAsync();
+        }
+    }
+}
